821a9e5cc8b0ca4bc0f89cbd903b12f0ccb1c7d2
Charles Cantrell <charles.e.cantrell@aexp.com>
2025-06-26 13:52:27 -0700

    uses a start fn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(`Hello from version ${process.env.npm_package_version}!`);
+const start = () => {
+  console.log(`Hello from version ${process.env.npm_package_version}!`);
+}
+
+start();
